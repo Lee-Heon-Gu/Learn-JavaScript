64fd4662b6695e09a7ded0257cb749ab6e2889da
Lee-Heon-Gu <gjsrn2006@naver.com>
2023-01-11 21:27:53 +0900

    second commit

--- 2_dom-control/1_selector.js
+++ 2_dom-control/1_selector.js
@@ -1,46 +1,45 @@
 // 1) get 메서드
-let title = document.getElementById('title');
+let title = document.getElementById('title'); // 아이디로 접근
 console.log(title);
 title.textContent = '헬스 3대 운동';
 
-let items = document.getElementsByClassName('item');
+let items = document.getElementsByClassName('item'); // 클래스 명으로 접근
 console.log('items');
 console.log('스쿼트', items[0]);
 console.log('벤치프레스', items[1]);
 console.log('데드리프트', items[2]);
 
-let liList = document.getElementsByTagName('li');
+let liList = document.getElementsByTagName('li'); // 태그 명으로 접근
 console.log(liList);
 
 // 2) HTML 요소 쿼리
-let h2 = document.querySelector('#title');
+// let h2 = document.querySelector('#title'); // 일치하는 요소 하나만을 반환.
 console.log(h2);
 
 let item = document.querySelector('.item');
 console.log(item);
 
-let itemAll = document.querySelectorAll('.item');
+let itemAll = document.querySelectorAll('.item'); // 일치하는 요소 목록을 반환.
 console.log('itemAll', itemAll);
 console.log('itemAll[0]', itemAll[0]);
 console.log('itemAll[1]', itemAll[1]);
 console.log('itemAll[2]', itemAll[2]);
 
-// h2.textContent = '<span>운동!!</span>';
-// h2.innerHTML= '<span>운동!!!</span>';
+h2.textContent = '<span>운동!!</span>';
+h2.innerHTML= '<span>운동!!!</span>';   
 
-// document.querySelector('body').innerHTML = '<strong>운동</strong>';
-
+document.querySelector('body').innerHTML = '<strong>운동</strong>';
+                                                                
 let input = document.querySelector('input');
-input.setAttribute('placeholder', '헬스 운동을 입력해주세요!');
-input.setAttribute('required', '');
-input.removeAttribute('placeholder');
-input.removeAttribute('required');
-// input
+input.setAttribute('placeholder', '헬스 운동을 입력해주세요!'); // placeholder 추가.
+input.setAttribute('required', ''); // required 추가.
+input.removeAttribute('placeholder'); // placeholder 삭제.
+input.removeAttribute('required'); // required 삭제.
 
 let helloItem = document.querySelector('.hello');
 console.log('helloItem', helloItem);
-// helloItem.style.color = 'blue';
-// helloItem.style.backgroundColor = 'black';
+helloItem.style.color = 'blue';             // 요소 글자색 blue로 변경.
+helloItem.style.backgroundColor = 'black';  // 요소 배경색 black으로 변경.
 
-helloItem.classList.add('dark','one');
-helloItem.classList.remove('dark');
+helloItem.classList.add('dark','one');      // 요소 클래스 'dark', 'one' 추가.
+helloItem.classList.remove('dark');         // 요소 클래스 'dark' 삭제.
--- 2_dom-control/2_dynamic-element.js
+++ 2_dom-control/2_dynamic-element.js
@@ -1,16 +1,26 @@
 let addBtn = document.getElementById('button');
 let addBeforeBtn = document.getElementById('before');
 let inputBox = document.querySelector('input');
+let removeTargetBtn = document.querySelector('#target-remove');
+let removeBtn = document.querySelector('.remove-btn');
 
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', function () {
     alert('추가');
     let li = document.createElement('li');
     let ul = document.querySelector('ul');
     li.textContent = inputBox.value;
+
+    let button = document.createElement('button');
+    button.textContent = 'X';
+    button.classList.add('remove-btn');
+    button.addEventListener('click', removeParentNode);
+    li.appendChild(button);
     ul.appendChild(li);
+    inputBox.value = '';
+    inputBox.focus();
 });
 
-addBeforeBtn.addEventListener('click', function() {
+addBeforeBtn.addEventListener('click', function () {
     alert("hello");
     let li = document.createElement('li');
     let ul = document.querySelector('ul');
@@ -19,4 +29,17 @@ addBeforeBtn.addEventListener('click', function() {
     li.textContent = inputBox.value;
     // ul.appendChild(li);
     ul.insertBefore(li, targetLi);
-})
+});
+
+removeTargetBtn.addEventListener('click', function () {
+    let targetLi = document.querySelector('#target');
+    targetLi.remove();
+});
+
+removeBtn.addEventListener('click', removeParentNode);
+
+function removeParentNode(event) {
+    event.target.parentNode.remove();
+    inputBox.focus();
+    inputBox.value = '';
+}
